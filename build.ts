5c532aa1c36031184a9191d2cf4a4ed216ddbac6
Suyashtnt <suyashtnt@gmail.com>
2024-02-17 21:14:07 +0200

    feat: base16 in yaml + nix

--- build.ts
+++ build.ts
@@ -1,5 +1,12 @@
 import { darkTheme, toHex } from "./mod.ts"
+import * as YAML from "yaml"
+ 
+const dark = toHex(darkTheme);
 
 // export dark.json
-const darkJson = JSON.stringify(toHex(darkTheme));
+const darkJson = JSON.stringify(dark);
 await Deno.writeTextFile("build/dark.json", darkJson);
+
+// export dark-base16.yaml
+const darkYaml = YAML.stringify(dark);
+await Deno.writeTextFile("build/dark-base16.yaml", darkYaml);
--- flake.nix
+++ flake.nix
@@ -13,6 +13,9 @@
 
       base16-lib = pkgs.callPackage base16.lib {};
       dark-theme-json = builtins.fromJSON (builtins.readFile ./build/dark.json);
+      dark-theme-base16 = {
+        inherit (dark-theme-json) base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F;
+      };
       dark-theme = base16-lib.mkSchemeAttrs dark-theme-json;
     in {
       devShells.default = pkgs.mkShell {
@@ -22,6 +25,7 @@
       themes = {
         dark = {
           base16-nix = dark-theme;
+          base16 = dark-theme-base16;
           json = dark-theme-json;
         };
       };
