9c635fb1340e4b60198377137059cc8acb3970b9
Suyashtnt <suyashtnt@gmail.com>
2024-06-26 18:19:29 +0200

    feat(zed): add basic autogenerated theme

---
+++ exporters/zed.ts
@@ -0,0 +1,16 @@
+import { render } from 'mustache'
+import { HumanPalette } from "../palletes.ts";
+import { toHex } from "./hex.ts";
+
+export const toVscodeTheme = async (pallete: HumanPalette) => {
+    const hex = toHex(pallete)
+    const template = await Deno.readTextFile('templates/zed.mustache')
+    const theme = Object.fromEntries(
+        Object.entries(hex).map(([key, value]) => [key + '-hex', value.replace?.("#", "")])
+    )
+
+    return render(template, {
+        'scheme-name': "Kleur",
+        ...theme
+    })
+}
